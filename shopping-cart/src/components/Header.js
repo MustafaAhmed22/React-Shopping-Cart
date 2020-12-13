010fb3ca80ad361665bc7fc21ec98cd1e816664e
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2020-12-12 17:09:22 -0800

    header

--- shopping-cart/src/components/Header.js
+++ shopping-cart/src/components/Header.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
 export default function Header(props){
-return <div>header</div>
+return (
+    <header className='row block center'>
+        <div className='font'>
+                <a href='#/'>
+                 My Shopping Cart
+                </a>
+        </div>
+        <div>
+            <a href= '#/cart'>Cart</a> <a href= '#/signin'>SignIn</a>
+
+        </div>
+    </header>
+)
 }
